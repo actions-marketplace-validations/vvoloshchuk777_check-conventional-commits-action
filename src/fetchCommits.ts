116cc848ce151d127ba57a94af4c65d821811245
Viacheslav Voloshchuk <vvoloshchuk777@gmail.com>
2023-03-20 15:21:57 +0100

    test

--- src/fetchCommits.ts
+++ src/fetchCommits.ts
@@ -1,17 +1,16 @@
-import get from 'lodash.get';
 import axios from 'axios';
 import { Commit } from './types';
 
 const fetchCommits = async (context): Promise<Commit[]> => {
   // push commits
-  const commits = Array.isArray(get(context, 'payload.commits'));
+  const commits = Array.isArray(context?.payload?.commits);
   if (commits) {
     return context.payload.commits;
   }
 
   // PR commits
   // Get a list of commits from the GH API:
-  const commitsURL = get(context, 'payload.pull_request.commits_url');
+  const commitsURL = context.payload.pull_request.commits_url;
   if (commitsURL) {
     try {
       const { data } = await axios.get(commitsURL);
